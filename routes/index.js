e3257152181ee259ebedee604424551d31c04b99
Triceratroll <adrianmf06@gmail.com>
2022-10-16 21:26:07 +0200

    node info api

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
+// routes index.js
 var express = require('express');
+const lightning = require('lightning');
+const lnd = require('../ln/connect')
 var router = express.Router();
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/* GET Node info */
+router.get('/info', async function(req, res, next){
+  try { 
+
+    // obtener información del nodo
+    const info = await lightning.getWalletInfo({ lnd });
+
+    // mostramos la info en formato json
+    res.send(`
+      <h1>Node info</h1>
+      <pre>${JSON.stringify(info, null, 2)}</pre>
+    `);
+
+    next();
+
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = router;
